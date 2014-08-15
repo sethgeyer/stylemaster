7d3d093fbd12a960096c333475fcd64eb869a871
Seth Geyer <seth.geyer@gmail.com>
2014-08-15 17:17:24 -0600

    added examples and initial commit

---
+++ app/controllers/styles_controller.rb
@@ -0,0 +1,8 @@
+class StylesController < ApplicationController
+
+def index
+
+end
+
+
+end
---
+++ spec/controllers/styles_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe StylesController, :type => :controller do
+
+end
